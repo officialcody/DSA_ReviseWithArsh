cb99042344a4080221ae035693195167d8ebed4c
Sparsh Goel <33709657+officialcody@users.noreply.github.com>
2024-01-18 15:11:31 +0000

    Question 1

--- GoldmanSachs1.java
+++ GoldmanSachs1.java
@@ -1,33 +1,39 @@
+/**
+ * QUES 1 -> Minimize the maximum of two arrays
+ * 
+ * Company - Goldman Sachs 
+ * 
+ * #revisewitharsh 
+ * #6companies30dayschallenge
+ */
 class Solution {
+    // Greatest Common Divisor
+    long gcd(int a,int b){
+        return b == 0 ? a : gcd(b,a % b);
+    }
+
+    // Least Common Multiple
+    long calculateLCM(int a, int b) {
+        return (long) ((a/gcd(a,b)) * b);
+    }
+
+    // Calculate the maximum element of both arrays after minimizing the possible solutions for number of ways
     public int minimizeSet(int divisor1, int divisor2, int uniqueCnt1, int uniqueCnt2) {
-        long start = uniqueCnt1 + uniqueCnt2;
-        long end = (int) 1e10;
-        long lcm = lcm(divisor1, divisor2);
-        long answer = 0;
-        while(start <= end) {
+        long start = 1,end = (int)(2 * 1e9);
+        long ans = end;
+        long lcm = calculateLCM(divisor1, divisor2);
+        while(start <= end){
             long mid = start + (end - start)/2;
-
-            boolean a = (mid - mid/divisor1) >= uniqueCnt1;
-            boolean b = (mid - mid/divisor2) >= uniqueCnt2;
-            boolean c = (mid - mid/lcm) >= (uniqueCnt1 + uniqueCnt2);
-
+            boolean a = (mid - mid / divisor1) >= uniqueCnt1;
+            boolean b = (mid - mid / divisor2) >= uniqueCnt2;
+            boolean c = (mid - mid / lcm) >= (uniqueCnt1 + uniqueCnt2);
             if(a && b && c) {
-                answer = mid;
+                ans = Math.min(ans, mid);
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
-
-        return (int) answer;
-    }
-
-    public long lcm(int a, int b) {
-        return (a * b)/gcd(a, b);
-    }
-
-    public long gcd(int a, int b) {
-        if(a==0) return b;
-        return gcd(b%a, a);
+        return (int)ans;
     }
 }
