63b3f8a5b3ba7f23f36b5399948cf8d0c85bb7fe
Sparsh Goel <33709657+officialcody@users.noreply.github.com>
2024-01-25 18:15:35 +0000

    Adobe : longest word in dictionary through deleting

---
+++ Adobe2.java
@@ -0,0 +1,34 @@
+/**
+ * QUES 2 -> Longest word in dictionary through deleting (LEETCODE - 524)
+ * 
+ * Company - Adobe
+ * 
+ * #revisewitharsh 
+ * #6companies30dayschallenge
+ */
+class Solution {
+    public String findLongestWord(String s, List<String> dictionary) {
+        int maxLength = 0;
+        String maxWord = "";
+        for(String word : dictionary) {
+            int length = checkIfWordPresentInString(s, word);
+            if(length > maxLength || (length == maxLength && maxWord.compareTo(word) > 0)) {
+                maxLength = length;
+                maxWord = word;
+            }
+        }
+        return maxWord;
+    }
+
+    public int checkIfWordPresentInString(String s, String word) {
+        for(int i = 0, j = 0; i <= s.length() - 1; i++) {
+            if(s.charAt(i) == word.charAt(j)) {
+                j++;
+                if(j == word.length()) {
+                    return j;
+                }
+            }
+        }
+        return 0;
+    }
+}
