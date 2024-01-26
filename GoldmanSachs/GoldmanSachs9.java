0d42bef1b3af6298aa3cbb266e655af5d41bad06
Sparsh Goel <33709657+officialcody@users.noreply.github.com>
2024-01-26 16:34:12 +0000

    goldman: finding consecutive integers from a data stream

---
+++ GoldmanSachs/GoldmanSachs9.java
@@ -0,0 +1,57 @@
+/**
+ * QUES 8 -> Finding consecutive integers from a data stream (LEETCODE - 2526)
+ * 
+ * Company - Goldman Sachs 
+ * 
+ * #revisewitharsh 
+ * #6companies30dayschallenge
+ */
+
+
+/***
+ * ********** Solution 1: Using Queue
+ */
+class DataStream {
+    Queue<Integer> queue;
+    int value, k;
+    public DataStream(int value, int k) {
+        this.queue = new LinkedList<Integer>();
+        this.value = value;
+        this.k = k;
+    }
+    
+    public boolean consec(int num) {
+        if(num == this.value) {
+            this.queue.add(num);
+        } else {
+            this.queue = new LinkedList<>();
+        }
+        return this.queue.size() >= k;
+    }
+}
+
+/*********
+ * ***************** Solution 2: Using class/member variables
+ */
+// class DataStream {
+//     int currentStreak = 0;
+//     int value, k;
+//     public DataStream(int value, int k) {
+//         this.k = k;
+//         this.value = value;
+//     }
+//     public boolean consec(int num) {
+//         if (num != value) {
+//             currentStreak = 0;
+//         } else {
+//             currentStreak++;
+//         }
+//         return currentStreak >= k;
+//     }
+// }
+
+/**
+ * Your DataStream object will be instantiated and called as such:
+ * DataStream obj = new DataStream(value, k);
+ * boolean param_1 = obj.consec(num);
+ */
