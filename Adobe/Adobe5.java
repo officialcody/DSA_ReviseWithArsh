a51494e05dfc8bacae81c2843a312fcc2cb38150
Sparsh Goel <33709657+officialcody@users.noreply.github.com>
2024-02-09 17:48:57 +0000

    Adobe 5

---
+++ Adobe/Adobe5.java
@@ -0,0 +1,13 @@
+/**
+ * QUES 5 -> Airplane Seat Assignment Probability (LEETCODE - 1227)
+ * 
+ * Company - Adobe
+ * 
+ * #revisewitharsh 
+ * #6companies30dayschallenge
+ */
+class Solution {
+    public double nthPersonGetsNthSeat(int n) {
+        return n == 1 ? 1.0 : 0.5; 
+    }
+}
