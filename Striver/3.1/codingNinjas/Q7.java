8fd0219c9401c0b23c425b7b43f3cc0c5ee483bf
Sparsh Goel <sparshgoelt26@gmail.com>
2024-02-19 22:46:59 +0530

    Striver : 3.1 Coding Ninjas Q7

---
+++ Striver/3.1/codingNinjas/Q7.java
@@ -0,0 +1,20 @@
+/****
+ * Move zeros to end
+ * Link: https://www.codingninjas.com/studio/problems/ninja-and-the-zero-s_6581958?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+ */
+public class Solution {
+    public static int[] moveZeros(int n, int []a) {
+        int index = 0;
+        for(int i=0;i<n;i++) {
+            if(a[i] != 0) {
+                a[index] = a[i];
+                index++;
+            }
+        }
+
+        for(int i=index;i<n;i++) {
+            a[i] = 0;
+        }
+        return a;
+    }
+}
